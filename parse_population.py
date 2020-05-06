6aaf03109d864fc46a33a0759cee3e274a741a25
jose-cubero <cuberojose@gmail.com>
2020-05-06 17:34:34 +0200

    template for parse_population.py

--- parse_population.py
+++ parse_population.py
@@ -1,1 +1,11 @@
-EMPTY FILE
+# Covid19 Plot confirmed cases of covid-19
+__author__ = "Jose Cubero"
+__version__ = "0.2.0"
+
+import argparse
+import os.path
+import csv
+import matplotlib.pyplot as plt
+import numpy as np
+
+#TODO: add here the creation of the world population dictionary
