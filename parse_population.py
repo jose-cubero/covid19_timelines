d9a71c20990c145ae85abab3a3592d275ede4c39
jose-cubero <cuberojose@gmail.com>
2020-05-20 11:43:17 +0200

    all DFs use 'Country' as common index

--- parse_population.py
+++ parse_population.py
@@ -20,14 +20,17 @@ def get_world_pop(country_list=None):
         df = df[df['Country_Area'].isin(country_list)]
     # DEBUG
     df.sort_values('Country_Area').to_csv( './tmp/clean_world_pop_selected.csv', columns=['Country_Area'], index=False, header=False )
-    
+
+    df = df.rename(columns={'Country_Area': 'Country'}).set_index('Country')
     return df
 
 def get_clean_covid_data(data_set):
 
     # TODO: add arg check for validity ['confirmed', 'deaths', 'recovered']
     file_name = './input/time_series_covid19_' + data_set + '_global.csv'
-    df = pd.read_csv(file_name, )
+    df = pd.read_csv(file_name, parse_dates=True)
+    # print(df)
+    # exit(0)
     df = df.drop(columns= ['Lat','Long'])
 
     # Clean data s1: get rid of colony data :D ()
@@ -52,7 +55,9 @@ def get_clean_covid_data(data_set):
 
     # Clean data s2: merge multi-region countries
     df = df.drop(columns= ['Province/State'])
-    df = df.groupby('Country/Region', as_index=False).sum()
+    df = df.rename(columns={'Country/Region': 'Country'})
+    # df = df.groupby('Country', as_index=False).sum()
+    df = df.groupby('Country').sum() # IMPLICIT INDE CHANGE TO Country
 
     # Clean data s3: Modify some particular names...
     # to be fixed:    
@@ -63,14 +68,10 @@ def get_clean_covid_data(data_set):
         'West Bank and Gaza' : 'Palestine'
     }
 
-    for item in fix_these:
-        df.loc[ df['Country/Region'] == item, 'Country/Region'] = fix_these[item]
-
-    #df.to_csv(('./tmp/clean_' + data_set + '.csv'))
-    df.sort_values('Country/Region').to_csv('./tmp/clean_covid.csv', columns=['Country/Region'], index=False, header=False)
+    df = df.rename(index=fix_these)
 
     # print(df) #DEBUG
-
+    df.sort_index().to_csv('./tmp/clean_covid.csv', columns=[], header=False)
     return df
 
 # Preparation: close matplotlib windows..
@@ -86,20 +87,19 @@ recovered_df = get_clean_covid_data('recovered')
 population_df = get_world_pop(country_list)
 
 # DEBUG, get full dictionary to compare names..
-#mylist = confirmed_df['Country/Region'].astype(str).tolist()
+#mylist = confirmed_df['Country'].astype(str).tolist()
 #world_pop_df = get_world_pop(mylist)
 
 # PLOT 1: Net confirmed cases
-confirmed_net = confirmed_df[ confirmed_df['Country/Region'].isin(country_list) ]
-confirmed_net.set_index('Country/Region', inplace=True)
+confirmed_net = confirmed_df.loc[ country_list , : ]
+# TODO: remove # confirmed_net.set_index('Country', inplace=True)
 # print("DF1: confirmed_net")
 # print(confirmed_net)
 # print("\n\n")
 confirmed_net.T.plot(title='Confirmed Cases, Netto')
 
 #PLOT 2: Normalized confirmed cases (per 100 inhabitants)
-population_df = population_df.loc[ : , ['Country_Area', 'Population_2019'] ]
-population_df.set_index('Country_Area', inplace=True)
+population_df = population_df.loc[ : , ['Population_2019'] ]
 # print("DF2: population_df")
 # print(population_df)
 # print(population_df.dtypes)
@@ -110,24 +110,22 @@ confirmed_pop = confirmed_net.div(population_df['Population_2019'], axis=0)
 # print("\n\n")
 confirmed_pop.T.plot(title='Confirmed Cases, per 1000 inhabitants')
 
+
 #PLOT 3: Daily Increase of confirmed cases
 daily_increase = confirmed_net.diff(axis=1)
 # print(death_rate)
 daily_increase.T.plot(title='Confirmed Cases, Daily Increase')
 
 #PLOT 4: Net deaths
-deaths_net = deaths_df[ deaths_df['Country/Region'].isin(country_list) ]
-deaths_net.set_index('Country/Region', inplace=True)
-
+deaths_net = deaths_df.loc[ country_list , : ]
 deaths_net.T.plot(title='Deaths, Netto')
 
 #PLOT 5: Deaths per confirmed cases. (Death Rate)
 death_rate = (deaths_net / confirmed_net) *100
-# print(death_rate)
 death_rate.T.plot(title='Death-rate (%) = Net Deaths / Net Cases')
 
 # DataFrame.nsmallest(self, n, columns[, keep])
 
 # Plot
 plt.show()
-# exit(0)
+exit(0)
--- sample1.py
+++ sample1.py
@@ -9,52 +9,57 @@ country_list = ['Germany', 'France', 'Spain', 'Italy']
 df = pd.DataFrame(np.random.randn(4, 6), columns=list('ABCDEF'))
 df['Country'] = country_list
 
-print("!!!!! Before setting the index")
-print(df)
-print("\n")
-print(df['A'])
-print("\n")
-
-myindex = df[ df['Country'] == 'Germany'].index
-print("myindex = " + "df[ df['Country'] == 'Germany'].index" + "=" + str(myindex))
-# dfa2 = df.loc[ df[ df['Country'] == 'Germany' ].index , : ]
-dfa2 = df.loc[ myindex[0] , : ]
-print("df.loc[ myindex[0] , : ]")
-print(dfa2)
-print("\n")
-
-# dfa3 = df.loc[ df[ df['Country'] == 'Germany' ].index , : 
-dfa3 = df.loc[ df['Country'] == 'Germany' , : ]
-print( "df.loc[ df['Country'] == 'Germany' , : ]")
-print(dfa3)
-print("\n")
-
-dfa4 = df.loc[ 0: 2 ]
-print(dfa4)
-dfa5 = df[ 0:3 ]  # ugly confusing, do not use!
-print(dfa5)
-print(dfa4 == dfa5)
-print("\n")
-
-##############
-print("!!!!! After setting the index")
-df.set_index('Country', inplace=True)
-print(df)
-print("\n")
+# print("!!!!! Before setting the index")
+# print(df)
+# print("\n")
 # print(df['A'])
 # print("\n")
-dfb2 = df.loc[ 'Germany' , : ]
-print(dfb2)
-print("\n")
-dfb3 = df.loc[ ['Germany'] , : ]
-print(dfb3)
-print("\n")
 
-# dfb4 = df.loc[ 'Germany': 'Spain' ]
-# dfb5 = df[ 'Germany': 'Spain' ]  # ugly confusing, do not use!
-# print(dfb4)
-# print(dfb5)
-# print(dfb4 == dfb5)
+# myindex = df[ df['Country'] == 'Germany'].index
+# print("myindex = " + "df[ df['Country'] == 'Germany'].index" + "=" + str(myindex))
+# # dfa2 = df.loc[ df[ df['Country'] == 'Germany' ].index , : ]
+# dfa2 = df.loc[ myindex[0] , : ]
+# print("df.loc[ myindex[0] , : ]")
+# print(dfa2)
 # print("\n")
+
+dfa3 = df.loc[ df[df['Country'] == 'Germany'].index , : ]   #overkill in this case
+dfa4 = df.loc[ df['Country'] == 'Germany' , : ]             # BEST?!
+dfa6 = df[ df['Country'] == 'Germany' ]                     #OK..
+print(dfa3)
+print(dfa4)
+print(dfa6)
+# print("\n")
+
+# dfa4 = df.loc[ 0: 2 ]
+# print(dfa4)
+# dfa5 = df[ 0:3 ]  # ugly confusing, do not use!
+# print(dfa5)
+# print(dfa4 == dfa5)
+# print("\n")
+
+# ##############
+print("!!!!! After setting the index")
+df.set_index('Country', inplace=True)
+# print(df)
+# print("\n")
+# print(df['A'])
+# print("\n")
+# dfb7 = df[ ['A','C'] ]  # OK.. still prefer loc
+# print(dfb7)
+# dfb2 = df.loc[ 'Germany' , : ]
+# print(dfb2)
+# print("\n")
+# dfb3 = df.loc[ ['Germany'] , : ]
+# print(dfb3)
+# print("\n")
+
+dfb4 = df.loc[ 'Germany': 'Spain', : ]  # OK
+dfb5 = df.loc[ ['Germany','Spain'], : ] # OK
+dfb6 = df[ 'Germany': 'Spain' ]         # TODO: UGLY confusing, do not use!
+print(dfb4)
+print(dfb5)
+print(dfb6)
+print("\n")
 
 exit(0)
