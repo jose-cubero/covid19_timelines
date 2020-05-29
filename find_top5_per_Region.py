45fd0afd9d7938c3f1f6c1305f6ead534e44873c
jose-cubero <cuberojose@gmail.com>
2020-05-29 10:20:35 +0200

    find_top: using pandas to plot the confirmed cases for the top 5 countries per region

--- find_top5_per_Region.py
+++ find_top5_per_Region.py
@@ -76,13 +76,80 @@ recovered_net_df = get_clean_covid_data('recovered')
 population_df    = get_world_pop()
 
 cases_norm_df    = cases_net_df.div(population_df['Population_2019'], axis=0) * 1000 # (per 1000)
+# drops countries without data..
+cases_norm_df.dropna(inplace=True)
+###### alternative, fill with zeroes.
+## cases_norm_df = cases_net_df.div(population_df['Population_2019'], axis=0, fillvalue=float) * 1000 # (per 1000) -> fill_na not implemented!
+## cases_norm_df.fillna(0)
+
 cases_delta_df   = cases_net_df.diff(axis=1)
+# TODO: apply a moving average to smooth the curve
 death_rate_df    = (deaths_net_df / cases_net_df) *100 # (%)
 
-sample_print = cases_norm_df.loc[ ['Costa Rica'] , :].T
-sample_print.plot(title='Death-rate (%) = Net Deaths / Net Cases')
+# Join region information as first column.
+#cases_norm_df = population_df.loc[: , ["UN_Region"]].join(cases_norm_df, how='inner')
 
-# DataFrame.nsmallest(self, n, columns[, keep])
+cases_net_df = pd.merge(population_df.loc[: , ["UN_Region"]], cases_net_df, left_index=True, right_index=True, how='inner')
+
+region_list = ["Northern America",
+               "Central America",
+               "Caribbean",
+               "South America",
+               "Northern Europe",
+               "Western Europe",
+               "Southern Europe",
+               "Eastern Europe",
+               "Northern Africa",
+               "Western Africa",
+               "Eastern Africa",
+               "Middle Africa",
+               "Southern Africa",
+               "Western Asia",
+               "Central Asia",
+               "Eastern Asia",
+               "Southern Asia",
+               "South-eastern Asia",
+               "Australia and New Zealand",
+               "Melanesia",
+            ]
+
+cat_type = pd.CategoricalDtype(categories=region_list, ordered=True)
+
+#cases_norm_df['UN_Region'] = cases_norm_df['UN_Region'].astype('category')
+cases_net_df['UN_Region'] = cases_net_df['UN_Region'].astype(cat_type)
+#pd.set_option("display.max_columns", 101)
+#print(cases_norm_df['UN_Region'].unique())
+
+grouped_data = cases_net_df.groupby("UN_Region")
+# print(a_group)
+# for name, group in a_group:
+#  	print(name)
+#  	print(group)
+# 	print("\n")
+
+# Find countries with more infections, per region
+my_llist = {}
+for region, dfx in grouped_data:
+    print("********" + region + "********")
+#    print("Total cases"+ dfx.sum)
+    print("total cases = ", dfx.iloc[:, -1].sum())
+    my_list= dfx.nlargest(5, columns=dfx.columns[-1], keep='all')
+    # print(my_list)
+    print(my_list.iloc[:,-1])
+    my_list = my_list.iloc[:,1:].div(population_df['Population_2019'], axis=0) * 1000 # (per 1000)
+    my_list.dropna(inplace=True)
+    (my_list.iloc[:, 1:]).T.plot(title=region)
+# 	print("\n")
+
+# my_list= cases_norm_df.nlargest(20, columns=cases_norm_df.columns[-1], keep='all')
+#mylist = confirmed_df['Country'].astype(str).tolist()
+# print(my_list)
+
+# (my_list.iloc[:, 1:]).T.plot(title='Normalized Cases (per 1000 inhabitants')
+
+# sample_print = cases_norm_df.loc[ ['Costa Rica'] , :].T
+# sample_print.plot(title='Death-rate (%) = Net Deaths / Net Cases')
+
 
 # GENERATE PLOTS
 # Preparation: close matplotlib windows..
