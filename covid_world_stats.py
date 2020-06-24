6c50ebffa3dacdb1b9a79093096a5e0cf6f00cb9
jose-cubero <cuberojose@gmail.com>
2020-06-24 15:57:28 +0200

    minor changes

--- covid_world_stats.py
+++ covid_world_stats.py
@@ -8,26 +8,26 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # local libs
-import pandas_main as covid_data
+import plot_covid_stats as pcovid
 
 # Load data into the DFs
-cases_net_df     = covid_data.world_confirmed_df
-deaths_net_df    = covid_data.world_deaths_df
-recovered_net_df = covid_data.world_recovered_df
-population_df    = covid_data.world_confirmed_df
+cases_net_df     = pcovid.world_confirmed_df
+deaths_net_df    = pcovid.world_deaths_df
+recovered_net_df = pcovid.world_recovered_df
+population_df    = pcovid.world_population_df
 
 cases_norm_df    = cases_net_df.div(population_df['Population_2019'], axis=0) * 1000 # (per 1000)
 # drops countries without data..
 cases_norm_df.dropna(inplace=True)
 
+# applying a moving average (8 days) to smooth the curves
 cases_delta_df   = cases_net_df.diff(axis=1).rolling(axis=1, window=8).mean()
-# TODO: apply a moving average to smooth the curve
 death_rate_df    = (deaths_net_df / cases_net_df).rolling(axis=1, window=8).mean() *100
 
 # Join region information as first column.
 #cases_norm_df = population_df.loc[: , ["UN_Region"]].join(cases_norm_df, how='inner')
 
-cases_net_df = pd.merge(population_df.loc[: , ["UN_Region"]], cases_net_df, left_index=True, right_index=True, how='inner')
+cases_net_df = pd.merge(population_df.loc[: , ["UN_Region", "Continent"]], cases_net_df, left_index=True, right_index=True, how='inner')
 
 region_list = ["Northern America",
                "Central America",
@@ -58,7 +58,8 @@ cases_net_df['UN_Region'] = cases_net_df['UN_Region'].astype(cat_type)
 #pd.set_option("display.max_columns", 101)
 #print(cases_norm_df['UN_Region'].unique())
 
-grouped_data = cases_net_df.groupby("UN_Region")
+# grouped_data = cases_net_df.groupby("UN_Region")
+merged_data = cases_net_df.groupby("UN_Region").sum()
 # print(a_group)
 # for name, group in a_group:
 #  	print(name)
@@ -66,21 +67,27 @@ grouped_data = cases_net_df.groupby("UN_Region")
 # 	print("\n")
 
 # Find countries with more infections, per region
-for region, dfx in grouped_data:
-    print("********" + region + "********")
+# for region, dfx in grouped_data:
+#     print("********" + region + "********")
     # my_list= dfx.nlargest(5, columns=dfx.columns[-1], keep='all')
-    my_list= dfx
-    print(my_list)
-    print(my_list.iloc[:,-1])
-    my_list = my_list.iloc[:,1:].div(population_df['Population_2019'], axis=0) * 1000 # (per 1000)
-    my_list.dropna(inplace=True)
-    (my_list.iloc[:, 1:]).T.plot(title=region)
+    # my_list= dfx
+    # print(my_list)
+    # print(my_list.iloc[:,-1])
+    # my_list = my_list.iloc[:,1:].div(population_df['Population_2019'], axis=0) * 1000 # (per 1000)
+    # my_list.dropna(inplace=True)
+    # (my_list.iloc[:, 1:]).T.plot(title=region)
 
-    # TODO Create a row of totals
-    print("total cases = ", dfx.iloc[:, -1].sum())
+    # # TODO Create a row of totals
+    # print("total cases = ", dfx.iloc[:, -1].sum())
+
+    #
+    # pcovid.plot_covid_6vars(dfx.index.tolist())
+
+    # print ("\n")
 
 
-    print ("\n")
+merged_data.T.plot()
+
 
 # my_list= cases_norm_df.nlargest(20, columns=cases_norm_df.columns[-1], keep='all')
 #mylist = confirmed_df['Country'].astype(str).tolist()
