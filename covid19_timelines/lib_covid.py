3260d7e89062296ecc06f63a1e9e72edbeac118a
jose-cubero <cuberojose@gmail.com>
2020-08-24 23:36:35 +0200

    new structured using poetry

--- covid19_timelines/lib_covid.py
+++ covid19_timelines/lib_covid.py
@@ -1,9 +1,9 @@
-# Plot COVID-19  
+# Plot COVID-19
 __author__ = 'Jose Cubero'
 __version__ = '1.0.0'
 
 # python libs
-import argparse
+# import argparse
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -19,7 +19,7 @@ def get_clean_covid_data(data_set, country_list=None):
         print("error, data_set" + data_set + "does not exist")
         exit(5)
 
-    file_name = './data/JHU/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_' + data_set + '_global.csv'
+    file_name = './data/covid-19/JHU/time_series_covid19_' + data_set + '_global.csv'
     df = pd.read_csv(file_name, parse_dates=True)
     df = df.drop(columns= ['Lat','Long'])
 
@@ -164,3 +164,69 @@ def create_primary(cg_dict = {}, filter_list =[]):
 
     return primary
 
+def plot_covid_6vars(country_list=[], region=""):
+
+    if (region != ""):
+        print("selected region: " + region)
+        country_list += lwp.UN_region_dict[region]
+        print(country_list)
+       
+    country_list[:] = [x for x in country_list if x in world_confirmed_df.index]
+
+    if(country_list == []):
+        print("plot_covid_6vars: no valid country list..")
+        exit(1)
+
+    if(debug_lib):
+        print("Selected countries: ")
+        print(country_list)
+        print("\n")
+
+    # Filter World population info
+    # population_df = pcov.get_world_pop(country_list)
+    population_df = world_population_df.loc[ country_list, :]
+
+    ##### Fill in Covid-19 raw data frames
+    # confirmed_df = pcov.get_clean_covid_data('confirmed', country_list)
+    confirmed_df = world_confirmed_df.loc[ country_list, :]
+    # deaths_df    = pcov.get_clean_covid_data('deaths', country_list)
+    deaths_df = world_deaths_df.loc[ country_list, :]
+    #recovered_df = pcov.get_clean_covid_data('recovered', country_list)
+    recovered_df = world_recovered_df.loc[ country_list, :]
+
+    ##### Create derived Covid-19 data 
+    # deaths_df    = pcov.get_clean_covid_data('deaths', country_list)
+    deaths_df = world_deaths_df.loc[ country_list, :]
+    #recovered_df = pcov.get_clean_covid_data('recovered', country_list)
+    recovered_df = world_recovered_df.loc[ country_list, :]
+
+    ##### Create derived Covid-19 data 
+    # Normalized confirmed cases (per 1000 inhabitants)
+    confirmed_norm_df = (confirmed_df.div(population_df['Population_2019'], axis=0))*1000
+    # Active cases, (per 1000 inhabitants)
+    active_norm_df = confirmed_df - recovered_df - deaths_df
+    active_norm_df = (active_norm_df.div(population_df['Population_2019'], axis=0))*1000
+    # Daily Increase of confirmed cases (per 1000 inhabitants, moving average)
+    # confirmed_delta_df = confirmed_df.diff(axis=1)
+    # confirmed_norm_delta_df = confirmed_norm_df.diff(axis=1)
+    confirmed_norm_delta_df = confirmed_norm_df.diff(axis=1).rolling(axis=1, window=8).mean()
+    # Deaths per confirmed cases. (Mortality Rate %)
+    death_rate_df = (deaths_df / confirmed_df) *100
+
+    #### Plotting the data 
+    fig, axs = plt.subplots(2, 3, sharex=True,
+                            figsize = (17,10),
+                            constrained_layout=True)
+                        #  gridspec_kw={'hspace': 0.15, 'wspace': 0.15},
+
+    # Plot the data-frames
+    confirmed_df.T.plot(ax=axs[0,0], title='Total Confirmed Cases')
+    confirmed_norm_df.T.plot(ax=axs[1,0], title='Confirmed Cases norm (per 1k inhabitants)')
+   
+    active_norm_df.T.plot(ax=axs[0,1], title='Active Cases norm')
+    confirmed_norm_delta_df.T.plot(ax=axs[1,1], title='Daily New Confirmed norm, Smoothed')
+
+    deaths_df.T.plot(ax=axs[0,2], title='Total Deaths')
+    death_rate_df.T.plot(ax=axs[1,2], title='Death-rate (%) = Net Deaths / Net Cases')
+   
+    return fig
--- covid19_timelines/pull_data.sh
+++ covid19_timelines/pull_data.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-#git subtree add --prefix data/JHU/COVID-19 https://github.com/CSSEGISandData/COVID-19 master --squash
-git subtree pull --prefix data/JHU/COVID-19 https://github.com/CSSEGISandData/COVID-19 master --squash
+
+wget https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv -P data/covid-19/JHU/
+wget https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv -P data/covid-19/JHU/
+wget https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv -P data/covid-19/JHU/
+
+mv data/covid-19/JHU/time_series_covid19_recovered_global.csv.1 data/covid-19/JHU/time_series_covid19_recovered_global.csv
+mv data/covid-19/JHU/time_series_covid19_deaths_global.csv.1 data/covid-19/JHU/time_series_covid19_deaths_global.csv
+mv data/covid-19/JHU/time_series_covid19_confirmed_global.csv.1 data/covid-19/JHU/time_series_covid19_confirmed_global.csv
+
